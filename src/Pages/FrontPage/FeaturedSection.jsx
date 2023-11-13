f28a5caca056168b8bdb85fd825bf352a6d92a40
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-11-13 18:54:11 +0600

    redux

--- src/Pages/FrontPage/FeaturedSection.jsx
+++ src/Pages/FrontPage/FeaturedSection.jsx
@@ -1,22 +1,8 @@
-// import Product from "../Product/Product";
-
-// const product = {
-//   name: "iphone 12",
-//   price: "1500",
-//   img: [
-//     {
-//       url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq9VDq-nO7LPivGObxZ21_h5Rgvw9AUW-qtQxV20NaB02JQj63u0niFFwzAui_XPrujzY&usqp=CAU",
-//     },
-//   ],
-//   _id: "sam",
-// };
-
 const FeaturedSection = () => {
   return (
     <div className="md:px-14 px-4 py-16 max-w-screen-2xl mx-auto">
       <div className="text-center my-8">
         <h2 className="text-3xl mb-2">Featured Products</h2>
-        {/* <Product product={product} /> */}
       </div>
     </div>
   );
---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import { productReducer } from "./reducers/productReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  products: productReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export default store;
