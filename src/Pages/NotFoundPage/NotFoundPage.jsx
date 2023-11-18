5f629e7899587b299a5d9ee398f3ed34677066fe
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-11-18 13:59:32 +0600

    index.html

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -163,7 +163,7 @@ const NotFoundPage = () => {
         </p>
         <a
           href="#"
-          className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150"
+          className="flex items-center space-x-2 bg-blue-600 hover:bg-green-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150"
           title="Return Home"
         >
           <svg
