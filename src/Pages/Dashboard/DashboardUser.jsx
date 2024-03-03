a0bc743f4e1ea3ad9782bd97425175fad8bc6433
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-03-03 21:48:26 +0600

    dashboard

--- src/Pages/Dashboard/DashboardUser.jsx
+++ src/Pages/Dashboard/DashboardUser.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAppSelector } from "../../redux/hook";
+import About from "../About/About";
 
 const DashboardUser = () => {
+  const currentUser = useAppSelector((state) => state.user.currentUser);
+  const userId = currentUser?.user?.id;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/v1/own-product/${userId}`
+        );
+
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await res.json();
+        console.log(data.products);
+        setData(data.products);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+        // Handle error or set appropriate state indicating an error.
+      }
+    }
+
+    // Call the function only when userId is available
+    if (userId) {
+      getData();
+    }
+  }, [userId]);
+ 
   return (
     <>
       <main className="w-full">
-        <div className="grid mb-4 pb-10 w-full px-8 mx-4 rounded-3xl bg-gray-100 border-4 border-green-400">
+        <div className="grid w-full px-8 pb-10 mx-4 mb-4 bg-gray-100 border-4 border-green-400 rounded-3xl">
           <div className="grid grid-cols-12 gap-6">
             <div className="grid grid-cols-12 col-span-12 gap-6 xxl:col-span-9">
               <div className="col-span-12 mt-8">
@@ -15,14 +46,14 @@ const DashboardUser = () => {
                 </div>
                 <div className="grid grid-cols-12 gap-6 mt-5">
                   <a
-                    className="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                    className="col-span-12 transition duration-300 transform bg-white rounded-lg shadow-xl hover:scale-105 sm:col-span-6 xl:col-span-3 intro-y"
                     href="#"
                   >
                     <div className="p-5">
                       <div className="flex justify-between">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="h-7 w-7 text-blue-400"
+                          className="text-blue-400 h-7 w-7"
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
@@ -34,11 +65,11 @@ const DashboardUser = () => {
                             d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                           />
                         </svg>
-                        <div className="bg-green-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
+                        <div className="flex h-6 px-2 text-sm font-semibold text-white bg-green-500 rounded-full justify-items-center">
                           <span className="flex items-center">30%</span>
                         </div>
                       </div>
-                      <div className="ml-2 w-full flex-1">
+                      <div className="flex-1 w-full ml-2">
                         <div>
                           <div className="mt-3 text-3xl font-bold leading-8">
                             4.510
@@ -52,14 +83,14 @@ const DashboardUser = () => {
                     </div>
                   </a>
                   <a
-                    className="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                    className="col-span-12 transition duration-300 transform bg-white rounded-lg shadow-xl hover:scale-105 sm:col-span-6 xl:col-span-3 intro-y"
                     href="#"
                   >
                     <div className="p-5">
                       <div className="flex justify-between">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="h-7 w-7 text-yellow-400"
+                          className="text-yellow-400 h-7 w-7"
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
@@ -71,11 +102,11 @@ const DashboardUser = () => {
                             d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
                           />
                         </svg>
-                        <div className="bg-red-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
+                        <div className="flex h-6 px-2 text-sm font-semibold text-white bg-red-500 rounded-full justify-items-center">
                           <span className="flex items-center">30%</span>
                         </div>
                       </div>
-                      <div className="ml-2 w-full flex-1">
+                      <div className="flex-1 w-full ml-2">
                         <div>
                           <div className="mt-3 text-3xl font-bold leading-8">
                             4.510
@@ -89,14 +120,14 @@ const DashboardUser = () => {
                     </div>
                   </a>
                   <a
-                    className="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                    className="col-span-12 transition duration-300 transform bg-white rounded-lg shadow-xl hover:scale-105 sm:col-span-6 xl:col-span-3 intro-y"
                     href="#"
                   >
                     <div className="p-5">
                       <div className="flex justify-between">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="h-7 w-7 text-pink-600"
+                          className="text-pink-600 h-7 w-7"
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
@@ -114,11 +145,11 @@ const DashboardUser = () => {
                             d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
                           />
                         </svg>
-                        <div className="bg-yellow-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
+                        <div className="flex h-6 px-2 text-sm font-semibold text-white bg-yellow-500 rounded-full justify-items-center">
                           <span className="flex items-center">30%</span>
                         </div>
                       </div>
-                      <div className="ml-2 w-full flex-1">
+                      <div className="flex-1 w-full ml-2">
                         <div>
                           <div className="mt-3 text-3xl font-bold leading-8">
                             4.510
@@ -132,14 +163,14 @@ const DashboardUser = () => {
                     </div>
                   </a>
                   <a
-                    className="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                    className="col-span-12 transition duration-300 transform bg-white rounded-lg shadow-xl hover:scale-105 sm:col-span-6 xl:col-span-3 intro-y"
                     href="#"
                   >
                     <div className="p-5">
                       <div className="flex justify-between">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="h-7 w-7 text-green-400"
+                          className="text-green-400 h-7 w-7"
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke="currentColor"
@@ -151,11 +182,11 @@ const DashboardUser = () => {
                             d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
                           />
                         </svg>
-                        <div className="bg-blue-500 rounded-full h-6 px-2 flex justify-items-center text-white font-semibold text-sm">
+                        <div className="flex h-6 px-2 text-sm font-semibold text-white bg-blue-500 rounded-full justify-items-center">
                           <span className="flex items-center">30%</span>
                         </div>
                       </div>
-                      <div className="ml-2 w-full flex-1">
+                      <div className="flex-1 w-full ml-2">
                         <div>
                           <div className="mt-3 text-3xl font-bold leading-8">
                             4.510
@@ -170,42 +201,40 @@ const DashboardUser = () => {
                   </a>
                 </div>
               </div>
-              <div className="col-span-12 mt-5">
-                <div className="grid gap-2 grid-cols-1 lg:grid-cols-2">
-                  <div className="bg-white shadow-lg p-4" id="chartline"></div>
-                  <div className="bg-white shadow-lg" id="chartpie"></div>
-                </div>
-              </div>
-              <div className="col-span-12 mt-5">
-                <div className="grid gap-2 grid-cols-1 lg:grid-cols-1">
-                  <div className="bg-white p-4 shadow-lg rounded-lg">
-                    <h1 className="font-bold text-base">Table</h1>
+             
+              {
+                data && data.length > 0 ? (
+                  <>
+                  <div className="col-span-12 mt-5">
+                <div className="grid grid-cols-1 gap-2 lg:grid-cols-1">
+                  <div className="p-4 bg-white rounded-lg shadow-lg">
+                    <h1 className="text-base font-bold">Table</h1>
                     <div className="mt-4">
                       <div className="flex flex-col">
                         <div className="-my-2 overflow-x-auto">
-                          <div className="py-2 align-middle inline-block min-w-full">
-                            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg bg-white">
+                          <div className="inline-block min-w-full py-2 align-middle">
+                            <div className="overflow-hidden bg-white border-b border-gray-200 shadow sm:rounded-lg">
                               <table className="min-w-full divide-y divide-gray-200">
                                 <thead>
                                   <tr>
-                                    <th className="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase bg-gray-50">
                                       <div className="flex cursor-pointer">
                                         <span className="mr-2">
                                           PRODUCT NAME
                                         </span>
                                       </div>
                                     </th>
-                                    <th className="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase bg-gray-50">
                                       <div className="flex cursor-pointer">
-                                        <span className="mr-2">Stock</span>
+                                        <span className="mr-2">Price</span>
                                       </div>
                                     </th>
-                                    <th className="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase bg-gray-50">
                                       <div className="flex cursor-pointer">
                                         <span className="mr-2">STATUS</span>
                                       </div>
                                     </th>
-                                    <th className="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase bg-gray-50">
                                       <div className="flex cursor-pointer">
                                         <span className="mr-2">ACTION</span>
                                       </div>
@@ -213,17 +242,18 @@ const DashboardUser = () => {
                                   </tr>
                                 </thead>
                                 <tbody className="bg-white divide-y divide-gray-200">
-                                  <tr>
-                                    <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                      <p>Apple MacBook Pro 13</p>
+                                  {data?.map((item) => (
+                                    <tr key={item.id}>
+                                    <td className="px-6 py-4 text-sm leading-5 whitespace-no-wrap">
+                                      <p>{item.name}</p>
                                       <p className="text-xs text-gray-400">
                                         PC & Laptop
                                       </p>
                                     </td>
-                                    <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                      <p>77</p>
+                                    <td className="px-6 py-4 text-sm leading-5 whitespace-no-wrap">
+                                      <p>${item.price}</p>
                                     </td>
-                                    <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                    <td className="px-6 py-4 text-sm leading-5 whitespace-no-wrap">
                                       <div className="flex text-green-500">
                                         <svg
                                           xmlns="http://www.w3.org/2000/svg"
@@ -242,7 +272,7 @@ const DashboardUser = () => {
                                         <p>Active</p>
                                       </div>
                                     </td>
-                                    <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                    <td className="px-6 py-4 text-sm leading-5 whitespace-no-wrap">
                                       <div className="flex space-x-4">
                                         <a
                                           href="#"
@@ -270,7 +300,7 @@ const DashboardUser = () => {
                                         >
                                           <svg
                                             xmlns="http://www.w3.org/2000/svg"
-                                            className="w-5 h-5 mr-1 ml-3"
+                                            className="w-5 h-5 ml-3 mr-1"
                                             fill="none"
                                             viewBox="0 0 24 24"
                                             stroke="currentColor"
@@ -287,6 +317,8 @@ const DashboardUser = () => {
                                       </div>
                                     </td>
                                   </tr>
+                                  ))}
+                             
                                 </tbody>
                               </table>
                             </div>
@@ -297,6 +329,15 @@ const DashboardUser = () => {
                   </div>
                 </div>
               </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="flex items-center justify-center">
+                      <h1 className="text-xl ">You have no products</h1>
+                      </div>
+                  </>
+                )
+                }
             </div>
           </div>
         </div>
